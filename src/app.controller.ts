5be8fef06ee264aecd9cc2285e747681def7f904
Eze <sendezeamail@gmail.com>
2026-09-19 12:20:54 +0100

    feat(health check) : include health check endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,6 +7,10 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Public()
+  @Get()
+  homeUrl() {
+    return { message: 'Welcome to Election Eye NG' };
+  }
   @Get('health')
   healthMethod() {
     return { message: 'Application is up and running' };
